867e3980d197f96076f51fb71c8388db2e0bb934
@OSSDA <80329840+mgcnb666@users.noreply.github.com>
2023-12-23 14:21:37 +0800

    Delete abigen.sh

--- abigen.sh
+++
@@ -1,3 +0,0 @@
-forge inspect src/interfaces/IVault.sol:IVault abi > abi/Vault.json
-forge inspect src/lens/VelocoreLens.sol:VelocoreLens abi > abi/Lens.json
-forge inspect src/pools/constant-product/ConstantProductPool.sol:ConstantProductPool abi > abi/CPP.json
