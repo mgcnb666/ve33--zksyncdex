1b371c95597aeedbde0ee1166b3bba1f79464828
@OSSDA <80329840+mgcnb666@users.noreply.github.com>
2023-12-23 14:21:15 +0800

    Delete src directory

--- src/interfaces/IPool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.19;
-
-import "contracts/lib/Token.sol";
-
-interface IPool {
-    function poolParams() external view returns (bytes memory);
-}
