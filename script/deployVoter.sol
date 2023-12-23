3112c0c0f7158c0a423041e283bbe96b98e5d545
@OSSDA <80329840+mgcnb666@users.noreply.github.com>
2023-12-23 14:34:49 +0800

    Add files via upload

---
+++ script/deployVoter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import "../src/AdminFacet.sol";
+import "../src/SwapFacet.sol";
+import "../src/SwapAuxillaryFacet.sol";
+import "../src/pools/vc/LVC.sol";
+import "src/pools/vc/VeVC.sol";
+import "src/pools/linear-bribe/LinearBribeFactory.sol";
+import "src/pools/converter/WETHConverter.sol";
+import "src/pools/wombat/WombatPool.sol";
+import "src/MockERC20.sol";
+import "src/lens/Lens.sol";
+import "src/NFTHolderFacet.sol";
+import "src/InspectorFacet.sol";
+import "src/lens/VelocoreLens.sol";
+import "src/pools/constant-product/ConstantProductPoolFactory.sol";
+import "src/pools/constant-product/ConstantProductLibrary.sol";
+import "src/sale/VoterFactory.sol";
+
+//address constant oldVC = 0x85D84c774CF8e9fF85342684b0E795Df72A24908;
+address constant oldVeVC = 0xbdE345771Eb0c6adEBc54F41A169ff6311fE096F;
+
+contract UpgradeScript is Script {
+    function setUp() public {}
+
+    function run() public returns (IVault, VC, VeVC) {
+        uint256 deployerPrivateKey = vm.envUint("VELOCORE_DEPLOYER");
+        vm.startBroadcast(deployerPrivateKey);
+
+        VoterFactory vf = VoterFactory(0x4e6db87cd059E0F95Ce36508B89775Bc7aa995FC);
+        IERC20(0xAeC06345b26451bdA999d83b361BEaaD6eA93F87).approve(address(vf), 150000e18);
+        vf.deploy(0xe3CDa0A0896b70F0eBC6A1848096529AA7AEe9eE, 150000e18);
+        vm.stopBroadcast();
+    }
+}
